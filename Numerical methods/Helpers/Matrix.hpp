aa8ebf7be00a5a5742c05ffae9687697f025da90
Arseny <62566543+BAUMANEZ@users.noreply.github.com>
2021-09-14 23:46:49 +0300

    Arseny (#3)
    * Normal print
    
    * 1/2 done

--- Numerical methods/Helpers/Matrix.hpp
+++ Numerical methods/Helpers/Matrix.hpp
@@ -14,21 +14,69 @@ enum FileError {
 	vectorNotFound,
 };
 
+enum Method {
+	gauss,
+	qr,
+};
+
+template<
+	typename T,
+	typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+> 
+struct SwappingElements {
+	T first_, second_; 
+
+	SwappingElements(const T first, const T second) {
+		first_ = first; second_ = second;
+	}
+};
+
 template<
 	typename T,
 	typename = typename std::enable_if<std::is_arithmetic<T>::value, T>::type
 > 
 class Matrix {
+	typedef std::vector<T> Vector;
+	typedef std::vector<size_t> Indices;
+	typedef std::vector<SwappingElements<T>> Journal;
+
 private:
-	std::vector<T> matrix_ = {};
-	std::vector<T> rightvalues_ = {};
+	Journal swapJournal = {};
+	Indices rows_ = {};
+	Indices cols_ = {};
+	Vector matrix_ = {};
+	Vector rightvalues_ = {};
 	size_t systemSize;
 
-	//MARK: Convenient functions
-	std::vector<T> gaussMethod() {
+	void partialColumnSelection(const size_t startCol) {
+		size_t maxCol = startCol;
+		T maxValue = at(startCol, startCol);
+		for (size_t col = startCol + 1; col < systemSize; ++col) {
+			const T nextValue = atvalue(startCol, col);
+			if ( nextValue > maxValue ) {
+				maxValue = nextValue;
+				maxCol = col;
+			}
+		}
+		if (startCol != maxCol) {
+			swapJournal.push_back(SwappingElements<T>(startCol, maxCol));
+			std::swap(cols_[startCol], cols_[maxCol]);
+		}
+	}
+
+	void resetIndexation(Vector& vector) {
+		for (auto it = swapJournal.end() - 1; it >= swapJournal.begin(); --it) {
+			const size_t leftIndex = it->first_;
+			const size_t rightIndex = it->second_;
+			std::swap(vector[rightIndex], vector[leftIndex]);
+		}
+	}
+
+	//MARK: - Convenient functions (internal use)
+	Vector gaussMethod() {
 		for(size_t k = 0; k < systemSize - 1; ++k)
 		{
-		    //THERE WILL BE: checking elem a[k][k] != 0 OR finding main elem 
+		    partialColumnSelection(k);
 			for(size_t row = k + 1; row < systemSize; ++row)
 			{
 				T value = atvalue(row , k);
@@ -39,8 +87,7 @@ private:
 				rightvalues_[row] -= rightvalues_[k] * value / at(k , k); 
 			}
 		}
-
-		std::vector<T> solution(systemSize, 0.0);
+		Vector solution(systemSize, 0.0);
 		for (int row = systemSize - 1; row >= 0; --row)
 		{
 			T x = 0.0;
@@ -51,24 +98,25 @@ private:
 			} 
 			x /= at(row,row);
 			solution[row] = x;
-		}			
+		}	
+		resetIndexation(solution); //!!!	
 		return solution;
 	}
 
 public:
+	//MARK: - Shared functions
  	T& at(const size_t row, const size_t col) 
 	{
-		return matrix_[row * systemSize + col];
+		return matrix_[rows_[row] * systemSize + cols_[col]];
 	}
 
 	T atvalue(const size_t row, const size_t col) 
 	{
-		return matrix_[row * systemSize + col];
+		return matrix_[rows_[row] * systemSize + cols_[col]];
 	}
 
 	void print(int width = 7, int prec = 4) 
 	{
-		std::cout << "---------------------"<< std::endl;;
 		for (size_t i = 0; i < systemSize; ++i)
 			{			
 				std::cout << "| "; 
@@ -76,12 +124,17 @@ public:
 					std::cout << std::setw(width) << std::setprecision(prec)<< at(i, j) << " ";
 				std::cout << "| " << rightvalues_[i] << std::endl;
 			}
-		std::cout << "---------------------"<< std::endl;;
+		std::cout << std::endl;
 	}
 
-	std::vector<T> linearSolveGauss()
-	{
-		return gaussMethod();
+	Vector linearSolve(Method method) {
+		switch (method) {
+		case gauss:
+			return gaussMethod();
+			break;
+		case qr:
+			return {};
+		}
 	}
 
 	Matrix() {}
@@ -115,10 +168,13 @@ public:
 			}
 
 			matrix_.reserve(systemSize * systemSize);
+			rows_.reserve(systemSize);
+			cols_.reserve(systemSize);
 			rightvalues_.reserve(systemSize);
 			double value;
 			for (size_t i = 0; i < systemSize; ++i)
 			{
+				rows_.push_back(i); cols_.push_back(i);
 				for (size_t j = 0; j < systemSize; ++j)
 				{
 					iFile >> value;
@@ -142,7 +198,7 @@ public:
 					errorDescription = "Vector not found";
 			}
 			std::cerr << errorDescription << std::endl;
-			exit(-1488);
+			exit(5);
 		}
 	};
 
