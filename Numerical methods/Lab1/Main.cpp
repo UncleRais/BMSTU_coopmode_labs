1a502763d1834b998ac312ca6b9f849223d2c4cc
Arseny <arsenytokarev@yandex.ru>
2021-09-19 20:04:24 +0300

    prints

--- Numerical methods/Lab1/Main.cpp
+++ Numerical methods/Lab1/Main.cpp
@@ -6,6 +6,6 @@ int main(int argc, char** argv)
 	const auto result = MatrixAlgorithms<double>::linearSolve(matrix);
 	matrix.printsystem();
 	print(result, vect);
-	save<double>(result, "Lab1/data", "soludtion");
+	save<double>(result, "Lab1/data", "solution");
 	return 0;
 }
