8e5e85c76c2be00fd40778a72bf404c57a1fde1f
Arseny <arsenytokarev@yandex.ru>
2021-11-09 23:35:17 +0300

    results

--- Numerical methods/Lab3/main.cpp
+++ Numerical methods/Lab3/main.cpp
@@ -1,15 +1,17 @@
 #include "../Helpers/Foundation/Foundation.cpp"
 #include "../Helpers/Foundation/Math.cpp"
-#include "func20.cpp"
+#include "func23.cpp"
 
 int main(int argc, char** argv) 
 {
-	Grid elem(-1 , 1, 10);
-	elem.setunifgrid(func);
-	elem.calculateSpline(-1 , 1, 5000);
+	Grid elem(-5 , 5, 32);
+	// elem.setunifgrid(func);
+	elem.setchebishevgrid(func);
+	// elem.calculateLagrange(-1, 1, 1000);
+	elem.calculateSpline(-1 , 1, 1000);
 	//elem.setunifgrid(func);
 	// elem.calculateLagrange(-1 , 1, 1000);
-	elem.printGrid();
+	// elem.printGrid();
 	std::cout<< "Function norm: " << elem.funcnorm(func) << "\n";
 	const std::string var = "20";
 	save(elem , "Lab3/output" , "vecx" + var, "vecy" + var, "vecintery" + var);
